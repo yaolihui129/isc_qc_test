b0402c2f358d50c33e2923bf0d7341a372e6d4e0
yaolihui129 <yaolihui129@sina.com>
2018-04-02 22:48:42 +0800

    整合秀丽广告

--- Apps/Xiuli/Controller/EmptyController.class.php
+++ Apps/Xiuli/Controller/EmptyController.class.php
@@ -2,8 +2,8 @@
 namespace Xiuli\Controller;
 use Think\Controller;
 class EmptyController  extends Controller {
-    public function index(){
-        
+    public function index()
+    {
         $this->error('您访问的网页不存在',U('/Xiuli/Index'));
     }
     
